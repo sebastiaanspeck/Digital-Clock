1e28211a8b1e576f413b64d59b8696930942b68c
Sebastiaan Speck <1420194speck@zuyd.nl>
2018-01-16 12:51:59 +0100

    Auto-Format
    Auto-formatted the code

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -11,46 +11,46 @@
 LiquidCrystal lcd(11, 12, 2, 3, 4, 5);
 
 // constants won't change:
-const String days[2][7] = {{"Sun","Mon", "Tue", "Wed", "Thu", "Fri", "Sat"},{"zo","ma", "di", "wo", "do", "vr", "za"}};
-const String months[2][12] = {{"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"},{"jan","feb","mrt","mei","jun","jul","aug","sep","okt","nov","dec"}};
-const String translations[2][3] = {{"Temp","D","Wk"},{"Temp","D","Wk"}};
+const String days[2][7] = {{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"}, {"zo", "ma", "di", "wo", "do", "vr", "za"}};
+const String months[2][12] = {{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}, {"jan", "feb", "mrt", "mei", "jun", "jul", "aug", "sep", "okt", "nov", "dec"}};
+const String translations[2][3] = {{"Temp", "D", "Wk"}, {"Temp", "D", "Wk"}};
 
 /* EXPLANATION DIFFERENT FUNCTIONS FOR CLOCK (WILL ONLY BE USED ON THE HOMEPAGE)
- * TIME
-'h' = hours
-'m' = minutes
-'s' = seconds
-'a' = alarm 1       (not atm)
-'A' = alarm 2       (not atm)
+   TIME
+   'h' = hours
+   'm' = minutes
+   's' = seconds
+   'a' = alarm 1       (not atm)
+   'A' = alarm 2       (not atm)
+   
+   WEATHER
+   'T' = temperature
+   'H' = humidity      (no sensor atm)
+   'P' = dew point     (no sensor atm)
 
- * WEATHER
-'T' = temperature
-'H' = humidity      (no sensor atm)
-'P' = dew point     (no sensor atm)
+   DATE
+   'd' = day of week
+   'D' = day
+   'M' = month
+   'S' = month (short string)
+   'Y' = year
+   'w' = weeknumber
+   'n' = daynumber
+   
+   MISCELLANEOUS
+   'l' = current location  (need to create a array of enums/strings which can be used in Settings)
 
- * DATE
-'d' = day of week
-'D' = day
-'M' = month
-'S' = month (short string)
-'Y' = year
-'w' = weeknumber
-'n' = daynumber
-
- * MISCELLANEOUS
-'l' = current location  (need to create a array of enums/strings which can be used in Settings)
-
- * DELIMTERS
-':' = delimiter
-'-' = delimiter
-'/' = delimiter
-'.' = delimiter
-'|' = delimiter
-' ' = delimiter
+   DELIMTERS
+   ':' = delimiter
+   '-' = delimiter
+   '/' = delimiter
+   '.' = delimiter
+   '|' = delimiter
+   ' ' = delimiter
 */
-char homepage[4][16] = {{"h:m:s"},{"d D-M-Y"},{"T"},{"w n"}};
+char homepage[4][16] = {{"h:m:s"}, {"d D-M-Y"}, {"T"}, {"w n"}};
 
-enum languages_t {EN, NL}; 
+enum languages_t {EN, NL};
 
 typedef struct {
   int hourFormat;               // 12 or 24 hour format (AM/PM is not displayed)
@@ -61,8 +61,8 @@ typedef struct {
   long switchPages;             // interval at which to switchPage 1 to 2 (milliseconds)
 } Settings;
 
-Settings default_settings = {24,NL,'c',true,1000,30000};
-Settings settings = {24,NL,'c',true,1000,30000};
+Settings default_settings = {24, NL, 'c', true, 1000, 30000};
+Settings settings = {24, NL, 'c', true, 1000, 30000};
 
 //TimeSettings settings_DST = {"MDT", Last, Sun, Mar, 2, 2};
 //TimeSettings settings_STD = {"MST", Last, Sun, Oct, 2, 1};
@@ -71,8 +71,8 @@ Settings settings = {24,NL,'c',true,1000,30000};
 //TimeChangeRule myDST = {settings_DST.abbrev, settings_DST.wk, settings_DST.dow, settings_DST.mon, settings_DST.hour, settings_DST.offset*60};    //Daylight time/Summertime = UTC + 2 hours
 //TimeChangeRule mySTD = {settings_STD.abbrev, settings_STD.wk, settings_STD.dow, settings_STD.mon, settings_STD.hour, settings_STD.offset*60};    //Standard time/Wintertime = UTC + 1 hours
 
-TimeChangeRule myDST = {"MDT", Last, Sun, Mar, 2, 2*60};    //Daylight time/Summertime = UTC + 2 hours
-TimeChangeRule mySTD = {"MST", Last, Sun, Oct, 2, 1*60};    //Standard time/Wintertime = UTC + 1 hours
+TimeChangeRule myDST = {"MDT", Last, Sun, Mar, 2, 2 * 60};  //Daylight time/Summertime = UTC + 2 hours
+TimeChangeRule mySTD = {"MST", Last, Sun, Oct, 2, 1 * 60};  //Standard time/Wintertime = UTC + 1 hours
 Timezone myTZ(myDST, mySTD);
 TimeChangeRule *tcr;        //pointer to the time change rule, use to get TZ abbrev
 
@@ -81,22 +81,22 @@ unsigned long oldMillis = 0;             // will store last time lcd switched pa
 int language_id;
 int rowX = 0;
 int rowY = 2;
-int numberOfPages = (sizeof(homepage)/sizeof(char))/32;
+int numberOfPages = (sizeof(homepage) / sizeof(char)) / 32;
 
 void setup() {
   Serial.begin(9600);
-  
+
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  
+
   // setSyncProvider() causes the Time library to synchronize with the
   // external RTC by calling RTC.get() every five minutes by default.
-  setSyncProvider(RTC.get);        
+  setSyncProvider(RTC.get);
   if (timeStatus() != timeSet) lcd << ("RTC SYNC FAILED");
 
-  pinMode(13,OUTPUT);
-  digitalWrite(13,LOW);
-  
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+
 }
 
 void loop() {
@@ -108,21 +108,21 @@ void loop() {
   if (currentMillis - previousMillis >= settings.intervalPage1) {
     // save the last time you refreshed the lcd
     previousMillis = currentMillis;
-    
+
     // display the date and time according to the specificied order with the specified settings
     displayPage(rowX, rowY);
   }
   if (currentMillis - oldMillis >= settings.switchPages) {
     oldMillis = currentMillis;
 
-    if(rowX == numberOfPages) {
+    if (rowX == numberOfPages) {
       rowX = 0;
       rowY = 2;
     } else {
       rowX += 2;
       rowY += 2;
     }
-    
+
   }
 }
 
@@ -133,16 +133,16 @@ void displayPage(int rowStart, int rowEnd) {
   local = myTZ.toLocal(utc, &tcr);
 
   // calculate which day and week of the year it is, according to the current local time
-  DayWeekNumber(year(local),month(local),day(local),weekday(local));
+  DayWeekNumber(year(local), month(local), day(local), weekday(local));
 
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   // for-loop which loops through each row
-  for(int row=rowStart;row<rowEnd;row++) {
+  for (int row = rowStart; row < rowEnd; row++) {
     // if row == 1, then we are on the second line, so move the cursor of the lcd
-    if(not(row%2) == 0) lcd.setCursor(0,1);
+    if (not(row % 2) == 0) lcd.setCursor(0, 1);
     // for-loop which loops through each char in row
-    for(int pos=0;pos<15;pos++) {
+    for (int pos = 0; pos < 15; pos++) {
       displayDesiredFunction(row, pos, local);
     }
   }
@@ -150,92 +150,92 @@ void displayPage(int rowStart, int rowEnd) {
 }
 
 void displayDesiredFunction(int row, int pos, time_t l) {
-  switch(homepage[row][pos]) {
+  switch (homepage[row][pos]) {
     case 'h':
       displayHours(l);            // display hours (use settings.hourFormat)
       break;
     case 'm':
       printI00(minute(l));        // display minutes
-      break;                      
+      break;
     case 's':
       printI00(second(l));        // display seconds
-      break;        
+      break;
     case 'T':
       displayTemperature();       // display temperature (use settings.temperatureFormat)
       break;
     case 'd':
       displayWeekday(weekday(l)); // display day of week (use settings.mondayFirstDay and lanuague)
-      break;        
+      break;
     case 'D':
       printI00(day(l));           // display day
-      break;  
+      break;
     case 'M':
       printI00(month(l));         // display month
       break;
     case 'S':
       displayMonthShortStr(month(l));    // display month as string
-      break;  
+      break;
     case 'Y':
       printI00(year(l));          // display year
-      break;  
+      break;
     case 'n':
       displayNumber('d');         // display daynumber
-      break;  
+      break;
     case 'w':
       displayNumber('w');         // display weeknumber
       break;
     case 'l':
-      displayLocation();            // display current location  
+      displayLocation();            // display current location
     case ':':
       displayDelimiter(':');      // display ':'
-      break;  
+      break;
     case '-':
       displayDelimiter('-');      // display '-'
-      break;  
+      break;
     case '/':
       displayDelimiter('/');      // display '/'
-      break;  
+      break;
     case '.':
       displayDelimiter('.');      // display '.'
       break;
     case '|':
       displayDelimiter('|');      // display '|'
-      break;    
+      break;
     case ' ':
       displayDelimiter(' ');      // display ' '
-      break;  
+      break;
   }
 }
 
 void displayHours(time_t l) {
-  (settings.hourFormat==24) ? printI00(hour(l)) : printI00(hourFormat12(l));
+  (settings.hourFormat == 24) ? printI00(hour(l)) : printI00(hourFormat12(l));
   return;
 }
 
 void displayTemperature() {
   int tem = RTC.temperature();
-  if(settings.labels) {
+  if (settings.labels) {
     lcd << translations[language_id][0] << ": ";
   }
-  (settings.degreesFormat == 'c') ? lcd <<int(tem / 4.0) << (char)223 << "C " : lcd << int(tem / 4.0 * 9.0 / 5.0 + 32.0) << (char)223 << "F ";
+  (settings.degreesFormat == 'c') ? lcd << int(tem / 4.0) << (char)223 << "C " : lcd << int(tem / 4.0 * 9.0 / 5.0 + 32.0) << (char)223 << "F ";
   return;
 }
 
 void displayWeekday(int val)
 {
-  lcd << days[language_id][val-1];
+  lcd << days[language_id][val - 1];
   return;
 }
 
 void displayNumber(char val) {
-  if(val == 'd') {
-    if(settings.labels == true) {
+  if (val == 'd') {
+    if (settings.labels == true) {
       lcd << translations[language_id][1] << ": ";
     }
     printI00(DW[0]);
   }
   else {
-    if(settings.labels == true) {
+    if (settings.labels == true) {
       lcd << translations[language_id][2] << ": ";
     }
     printI00(DW[1]);
@@ -253,7 +253,7 @@ void displayLocation() {
 }
 
 void defineLanguageId() {
-  switch(settings.language) {
+  switch (settings.language) {
     case EN:
       language_id = 0;
       break;
@@ -276,7 +276,7 @@ void displayDelimiter(char delim) {
 
 void setTimeRTC(unsigned int hours, unsigned int minutes, unsigned int seconds, unsigned int d, unsigned int m, unsigned int y) {
   setTime(hours, minutes, seconds, d, m, y);
-  RTC.set(now()); 
+  RTC.set(now());
   return;
 }
 
